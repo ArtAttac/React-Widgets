b9e9eb66722dcc2a2a5e448ce1fd16ea65d8101e
Art I <sovietbulldozer@gmail.com>
2020-08-31 17:18:08 -0400

    Updates

---
+++ precache-manifest.db365ca483fc1aeda2718c72194477e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e61c9821f195e9d4536ea16902cada65",
+    "url": "/react-widgets/index.html"
+  },
+  {
+    "revision": "fe4336911d388489be86",
+    "url": "/react-widgets/static/js/2.8613df85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-widgets/static/js/2.8613df85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "609962700bdaf492c57d",
+    "url": "/react-widgets/static/js/main.adcaf098.chunk.js"
+  },
+  {
+    "revision": "26825fa57403bcd092d3",
+    "url": "/react-widgets/static/js/runtime-main.4c274bfd.js"
+  }
+]);
